9060317d07555ba9b3dcf46d10d00046f0753d83
ferizoozoo <farhadzohoorz@gmail.com>
2020-07-17 14:29:18 +0430

    Shift characters by the specified key in the alphabet

--- Encryption.java
+++ Encryption.java
@@ -2,18 +2,23 @@ package encryptdecrypt;
 
 import java.util.Scanner;
 
-public class Encryption {
+public class Main {
     public static void main(String[] args) {
-        String message = "we found a treasure!";
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+        int shift = scanner.nextInt();
         String cypheredMessage = "";
         for (int i = 0; i < message.length(); i++)
         {
             char character = message.charAt(i);
             if (!(character == ' ' || character == '!'))
-                cypheredMessage += (char)(219 - character);
+                cypheredMessage += alphabet.charAt((shift + (int)character - 97) % 26);
             else
                 cypheredMessage += character;    
         }
         System.out.println(cypheredMessage);
     }
 }
+
