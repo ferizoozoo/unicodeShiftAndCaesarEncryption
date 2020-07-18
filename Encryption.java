8e3946f91ecc492d5d218617d5ae2b097971637c
ferizoozoo <farhadzohoorz@gmail.com>
2020-07-18 17:28:03 +0430

    Create encrypt and decrypt functions

--- Encryption.java
+++ Encryption.java
@@ -2,23 +2,40 @@ package encryptdecrypt;
 
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        
-        Scanner scanner = new Scanner(System.in);
-        String message = scanner.nextLine();
-        int shift = scanner.nextInt();
+public class Main {    
+    public static String encrypt(String message, int key) {
         String cypheredMessage = "";
         for (int i = 0; i < message.length(); i++)
         {
             char character = message.charAt(i);
-            if (!(character == ' ' || character == '!'))
-                cypheredMessage += alphabet.charAt((shift + (int)character - 97) % 26);
-            else
-                cypheredMessage += character;    
+            cypheredMessage += (char)((int)character + key);    
         }
-        System.out.println(cypheredMessage);
+        return cypheredMessage;
+    }
+    
+    public static String decrypt(String cypheredMessage, int key) {
+        String message = "";
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < cypheredMessage.length(); i++)
+        {
+            char character = cypheredMessage.charAt(i);
+            message += (char)((int)character - key);  
+        }
+        return message;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        String message = scanner.nextLine();
+        int key = scanner.nextInt();
+        String output = "";
+        if (command.equals("enc")) {
+            output += encrypt(message, key);
+        } else if (command.equals("dec")) {
+            output += decrypt(message, key);
+        }
+        System.out.println(output);
     }
 }
 
