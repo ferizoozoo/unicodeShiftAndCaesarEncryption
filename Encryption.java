e1807ba22571b50ee886400325877c4096eb57e1
ferizoozoo <farhadzohoorz@gmail.com>
2020-07-24 14:09:19 +0430

    Add -in, -out command-line options for getting data from file or writing to a file

--- Encryption.java
+++ Encryption.java
@@ -1,21 +1,26 @@
 package encryptdecrypt;
 
+import java.io.FileDescriptor;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
 import java.util.Scanner;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 public class Main {    
+    // TODO: make the encrypt and decrypt methods, one method which receives an extra parameter named "mode"
     public static String encrypt(String message, int key) {
         String cypheredMessage = "";
-        for (int i = 0; i < message.length(); i++)
-        {
+        for (int i = 0; i < message.length(); i++) {
             char character = message.charAt(i);
-            cypheredMessage += (char)((int)character + key);    
+            cypheredMessage += (char) ((int) character + key);
         }
         return cypheredMessage;
     }
     
     public static String decrypt(String cypheredMessage, int key) {
         String message = "";
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
         for (int i = 0; i < cypheredMessage.length(); i++)
         {
             char character = cypheredMessage.charAt(i);
@@ -28,6 +33,8 @@ public class Main {
         String mode = "";
         String data = "";
         int key = 0;
+        String inFile = "";
+        String outFile = "";
         int counter = 0;
         while (counter < args.length) {
             switch (args[counter]) {
@@ -39,17 +46,43 @@ public class Main {
                     break;
                 case "-mode":
                     mode += args[++counter];
-                    break;            
+                    break;
+                case "-in":
+                    inFile += args[++counter];
+                    break;
+                case "-out":
+                    outFile += args[++counter];
+                    break;
             }
             counter++;
         }
+
+        File file = new File(inFile);
         String output = "";
+
+        if (data.equals("") && !inFile.equals("")) {
+            try (Scanner scanner = new Scanner(file)) {
+                data += new String(Files.readAllBytes(Paths.get(inFile)));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
         if (mode.equals("enc")) {
             output += encrypt(data, key);
         } else if (mode.equals("dec")) {
             output += decrypt(data, key);
         }
-        System.out.println(output);
+
+        if (outFile.equals("")) {
+            System.out.println(output);
+        } else {
+            try (FileWriter fileWriter = new FileWriter(outFile)) {
+                fileWriter.write(output);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
 
